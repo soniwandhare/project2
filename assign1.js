f0c6d1cd33a0053d65b3e9387ea4913e3b69b1f8
Your Name <you@example.com>
2024-12-01 10:07:37 +0530

    first commit

---
+++ assign1.js
@@ -0,0 +1,26 @@
+
+
+let mychangeimg = document.getElementById("changeimg")
+
+
+
+function changebtn1 () {
+
+    mychangeimg.src = "images/seasontr3.jpg";
+
+}
+
+function changebtn2 () {
+   
+    mychangeimg.src = "images/seasontr1.jpg" 
+}
+
+function changebtn3 () {
+    
+    mychangeimg.src = "images/seasontr4.jpg"
+}
+
+function changebtn4 () {
+    
+    mychangeimg.src = "images/seasontr2.jpg"
+}
